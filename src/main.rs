5e07a9bdd414f36f540f3721ae9d047bf4171df4
tsarcasticx <kautsarakbarardianta@gmail.com>
2025-07-29 18:23:45 +0700

    refractoring my main.rs due to clarity and my personal preference

--- src/main.rs
+++ src/main.rs
@@ -12,44 +12,38 @@ fn main() {
     loop {
         print!("What you are going to do?\n\x1B[33m[0] Exit \n[1] Exponential \n[2] Simple Factorial
 [3] Fibonacci pattern \n[4] Quartil Decil Percentil\n[5] Logarithm \x1B[37m \nEnter the index: ");
-        let _choice = user::user_input_string();
-    
-        if _choice == "0" {
-            println!("Thank you for partisipating!");
-            exit(0);
-        }
-        else if _choice == "1" { // then choosing exponential
-            print!("\n\nEnter the basis: ");
-            let basis: i32 = user::user_input_int();
-            print!("\nEnter the exponent: ");
-            let _expo: i32 = user::user_input_int();
-            let _basis: f64 = basis.into();
-            println!("\nThe result is {}\n", exponential::exponentialis(_basis, _expo));
-        }
-        else if _choice == "2" {
-            print!("\n\nEnter a number to be factorialized: ");
-            let mut _number: i128 = user::user_input_int().into();
-            println!("\nThe result is {}\n", factorial::factors(_number));
-        }
-        else if _choice == "3" {
-            let mut li:Vec<i128> = Vec::new();
-            print!("\n\nHow many do you want to reveal the pattern? ");
-            let _value: i128 = user::user_input_int().into();
-            println!("\nLoading...");
-            for i in 0.._value {
-                li.push(fibonacci::fibonaccis(i));
-            }
-            println!("\nThe pattern is {:?}", li);
-            li.pop();
-        }
-        else if _choice == "4" {
-            statistic::statistica();
-        }
-        else if _choice == "5" {
-            logarithm::logaritma();
-        }
-        else {
-                println!("You must input one of three numbers, not anything else");
+        let choice = user::user_input_string();
+        let _choice:&str = choice.as_str();
+
+        match _choice {
+            "0" => {println!("hallo"); exit(0);},
+            "1" => {
+                print!("\n\nEnter the basis: ");
+                let basis: i32 = user::user_input_int();
+                print!("\nEnter the exponent: ");
+                let _expo: i32 = user::user_input_int();
+                let _basis: f64 = basis.into();
+                println!("\nThe result is {}\n", exponential::exponentialis(_basis, _expo));
+            },
+            "2" => {
+                print!("\n\nEnter a number to be factorialized: ");
+                let mut _number: i128 = user::user_input_int().into();
+                println!("\nThe result is {}\n", factorial::factors(_number));
+            },
+            "3" => {
+                let mut li:Vec<i128> = Vec::new();
+                print!("\n\nHow many do you want to reveal the pattern? ");
+                let _value: i128 = user::user_input_int().into();
+                println!("\nLoading...");
+                for i in 0.._value {
+                    li.push(fibonacci::fibonaccis(i));
+                }
+                println!("\nThe pattern is {:?}", li);
+                li.pop();
+            },
+            "4" => statistic::statistica(),
+            "5" =>logarithm::logaritma(),
+            _ => println!("You must input one of three numbers, not anything else"),
         }
     }
 }
